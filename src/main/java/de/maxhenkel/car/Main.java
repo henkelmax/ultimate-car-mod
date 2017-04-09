080b745d9a5ebf92f5582d1fd4b47e2869aa7f3a
Max Henkel <mh@maxhenkel.de>
2017-04-09 16:12:55 +0200

    Incremented version

--- src/main/java/de/maxhenkel/car/Main.java
+++ src/main/java/de/maxhenkel/car/Main.java
@@ -13,9 +13,9 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class Main{
 	
     public static final String MODID = "car";
-    public static final String VERSION = "1.0.7";
+    public static final String VERSION = "1.0.8";
     public static final String MC_VERSION = "[1.10.2]";
-    public static final int VERSION_NUMBER = 8;
+    public static final int VERSION_NUMBER = 9;
 
 	@Instance
     private static Main instance;
