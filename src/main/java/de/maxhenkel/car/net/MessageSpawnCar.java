e7993760a343efb68a9ee9cb77dc030fc5ec5957
Max Henkel <max@maxhenkel.de>
2017-10-09 12:52:38 +0200

    Fixed car not spawning bug

--- src/main/java/de/maxhenkel/car/net/MessageSpawnCar.java
+++ src/main/java/de/maxhenkel/car/net/MessageSpawnCar.java
@@ -10,43 +10,48 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 import net.minecraftforge.fml.relauncher.Side;
 
-public class MessageSpawnCar implements IMessage, IMessageHandler<MessageSpawnCar, IMessage>{
+public class MessageSpawnCar implements IMessage, IMessageHandler<MessageSpawnCar, IMessage> {
 
 	private int posX;
 	private int posY;
 	private int posZ;
-	
+
 	public MessageSpawnCar() {
 	}
-	
+
 	public MessageSpawnCar(BlockPos pos) {
-		this.posX=pos.getX();
-		this.posY=pos.getY();
-		this.posZ=pos.getZ();
+		this.posX = pos.getX();
+		this.posY = pos.getY();
+		this.posZ = pos.getZ();
 	}
 
 	@Override
 	public IMessage onMessage(MessageSpawnCar message, MessageContext ctx) {
-		if(ctx.side.equals(Side.SERVER)){
-			EntityPlayer player=ctx.getServerHandler().player;
-			
-			TileEntity te=player.world.getTileEntity(new BlockPos(message.posX, message.posY, message.posZ));
-			
-			if(te instanceof TileEntityCarWorkshop){
-				TileEntityCarWorkshop carWorkshop=(TileEntityCarWorkshop) te;
-				
-				carWorkshop.spawnCar(player);
+		if (ctx.side.equals(Side.SERVER)) {
+			final EntityPlayer player = ctx.getServerHandler().player;
+
+			TileEntity te = player.world.getTileEntity(new BlockPos(message.posX, message.posY, message.posZ));
+
+			if (te instanceof TileEntityCarWorkshop) {
+				final TileEntityCarWorkshop carWorkshop = (TileEntityCarWorkshop) te;
+				//Scheduled task for spawning
+				player.getServer().addScheduledTask(new Runnable() {
+					public void run() {
+						carWorkshop.spawnCar(player);
+					}
+				});
+
 			}
-			
+
 		}
 		return null;
 	}
 
 	@Override
 	public void fromBytes(ByteBuf buf) {
-		this.posX=buf.readInt();
-		this.posY=buf.readInt();
-		this.posZ=buf.readInt();
+		this.posX = buf.readInt();
+		this.posY = buf.readInt();
+		this.posZ = buf.readInt();
 	}
 
 	@Override
