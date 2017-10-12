cfcfb0bd807f2134cab23277061c20a4cc073562
Max Henkel <max@maxhenkel.de>
2017-10-12 22:19:42 +0200

    Fixed MTLib bug

--- src/main/java/de/maxhenkel/car/Main.java
+++ src/main/java/de/maxhenkel/car/Main.java
@@ -16,7 +16,7 @@ public class Main{
     public static final String VERSION = "1.2.5";
     public static final String MC_VERSION = "[1.12.2]";
 	public static final String UPDATE_JSON = "http://maxhenkel.de/update/car.json";
-	public static final String DEPENDENCIES = "required-after:redstoneflux;after:dynamiclights";
+	public static final String DEPENDENCIES = "required-after:redstoneflux;after:dynamiclights;after:crafttweaker;";
 
 	@Instance
     private static Main instance;
--- src/main/java/de/maxhenkel/car/integration/modtweaker/CarWorkshop.java
+++ src/main/java/de/maxhenkel/car/integration/modtweaker/CarWorkshop.java
@@ -1,7 +1,6 @@
 package de.maxhenkel.car.integration.modtweaker;
 
 import java.util.UUID;
-import com.blamejared.mtlib.helpers.InputHelper;
 import crafttweaker.annotations.ModOnly;
 import crafttweaker.annotations.ZenRegister;
 import crafttweaker.api.item.IItemStack;
@@ -21,31 +20,42 @@ public class CarWorkshop {
 
 	@ZenMethod
 	public static void addRecipe(String carID, IItemStack[][] input/* , @Optional IItemStack secondaryOutput */) {
-		ICar car=CarRegistry.REGISTRY.getEntry(carID);
-		if(car==null) {
-			throw new IllegalArgumentException("Car with ID '" +carID +"' not found");
+		ICar car = CarRegistry.REGISTRY.getEntry(carID);
+		if (car == null) {
+			throw new IllegalArgumentException("Car with ID '" + carID + "' not found");
 		}
-		
-		if(input.length!=3||input[0].length!=5) {
+
+		if (input.length != 3 || input[0].length != 5) {
 			throw new IllegalArgumentException("Wrong pattern size");
 		}
-		
-		ItemStack[] stacks=new ItemStack[3*5];
-		int index=0;
-		for(int i=0; i<input.length; i++) {
-			for(int j=0; j<input[i].length; j++) {
-				stacks[index]=InputHelper.toStack(input[i][j]);
+
+		ItemStack[] stacks = new ItemStack[3 * 5];
+		int index = 0;
+		for (int i = 0; i < input.length; i++) {
+			for (int j = 0; j < input[i].length; j++) {
+				stacks[index] = toStack(input[i][j]);
 				index++;
 			}
 		}
-		
-		String s=UUID.randomUUID().toString();
+
+		String s = UUID.randomUUID().toString();
 		CarCraftingRegistry.REGISTRY.register(s, new ShapedCarRecipe(stacks, car));
 	}
 
 	@ZenMethod
 	public static void removeRecipe(String carID) {
 		CarCraftingRegistry.REGISTRY.unregister(carID);
+	}
+
+	public static ItemStack toStack(IItemStack iStack) {
+		if (iStack == null) {
+			return ItemStack.EMPTY;
+		}
+		Object internal = iStack.getInternal();
+		if (!(internal instanceof ItemStack)) {
+			throw new IllegalArgumentException("Failed to convert to ItemStack");
+		}
+		return (ItemStack) internal;
 	}
 
 }
