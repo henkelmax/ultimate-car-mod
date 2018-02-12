b35ab36e56e71a43523d790e40ed1429863e0753
Max Henkel <max@maxhenkel.de>
2018-02-12 18:48:50 +0100

    Add battery recharging

--- src/main/java/de/maxhenkel/car/items/ItemBattery.java
+++ src/main/java/de/maxhenkel/car/items/ItemBattery.java
@@ -1,9 +1,18 @@
 package de.maxhenkel.car.items;
 
+import cofh.redstoneflux.api.IEnergyProvider;
 import de.maxhenkel.car.ModCreativeTabs;
+import de.maxhenkel.car.blocks.ModBlocks;
+import de.maxhenkel.car.blocks.tileentity.TileEntityGenerator;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 
@@ -33,5 +42,26 @@ public class ItemBattery extends Item {
     @Override
     public boolean isEnchantable(ItemStack stack) {
         return false;
+    }
+
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+	    if(worldIn.getBlockState(pos).getBlock().equals(ModBlocks.GENERATOR)){
+            TileEntity te=worldIn.getTileEntity(pos);
+            if(te instanceof IEnergyProvider){
+                IEnergyProvider provider= (IEnergyProvider) te;
+                ItemStack stack=player.getHeldItem(hand);
+
+                int energyToFill=stack.getItemDamage();
+
+                int amount=provider.extractEnergy(facing, energyToFill, false);
+
+                stack.setItemDamage(energyToFill-amount);
+                player.setHeldItem(hand, stack);
+                return EnumActionResult.SUCCESS;
+            }
+        }
+
+	    return super.onItemUse(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
     }
 }
