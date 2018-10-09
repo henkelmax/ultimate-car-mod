0d271a204edcb446c0e869f86c02a9ef402e618d
Max Henkel <max@maxhenkel.de>
2018-10-09 20:56:53 +0200

    Update version to 1.2.13

--- src/main/java/de/maxhenkel/car/Main.java
+++ src/main/java/de/maxhenkel/car/Main.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class Main{
 	
     public static final String MODID = "car";
-    public static final String VERSION = "1.2.12";
+    public static final String VERSION = "1.2.13";
     public static final String MC_VERSION = "[1.12.2]";
 	public static final String UPDATE_JSON = "http://maxhenkel.de/update/car.json";
 	public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2768,);after:crafttweaker;";
