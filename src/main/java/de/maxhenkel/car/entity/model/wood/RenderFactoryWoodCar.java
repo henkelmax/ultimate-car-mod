044cfcdd3f5377b33bb797e85574b733005d4f8f
Max Henkel <max@maxhenkel.de>
2018-10-09 14:30:53 +0000

    Revert "Merge branch 'obj_test' into 'master'"
    This reverts commit 38a7a6399d634b8611d3be2966c760f72f15978b

--- src/main/java/de/maxhenkel/car/entity/model/wood/RenderFactoryWoodCar.java
+++ src/main/java/de/maxhenkel/car/entity/model/wood/RenderFactoryWoodCar.java
@@ -1,7 +1,6 @@
 package de.maxhenkel.car.entity.model.wood;
 
 import de.maxhenkel.car.entity.car.EntityCarWood;
-import de.maxhenkel.car.entity.model.TestCarModel;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
@@ -10,8 +9,7 @@ public class RenderFactoryWoodCar implements IRenderFactory<EntityCarWood>{
 
 	@Override
 	public Render<EntityCarWood> createRenderFor(RenderManager manager) {
-		//return new RenderWoodCar(manager);
-		return new TestCarModel(manager);
+		return new RenderWoodCar(manager);
 	}
 
 }
