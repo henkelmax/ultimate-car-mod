f359ef31b24e546010538477d2e61fee2a5a82f2
BuildTools <unconfigured@null.spigotmc.org>
2019-08-08 12:46:31 +0200

    Update jei and forge

--- src/main/java/de/maxhenkel/car/events/PlayerEvents.java
+++ src/main/java/de/maxhenkel/car/events/PlayerEvents.java
@@ -7,71 +7,71 @@ import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 
 @OnlyIn(Dist.CLIENT)
 public class PlayerEvents {
 
-	private Minecraft minecraft;
-	private EntityVehicleBase lastVehicle;
+    private Minecraft minecraft;
+    private EntityVehicleBase lastVehicle;
 
-	public PlayerEvents() {
-		this.minecraft = Minecraft.getInstance();
-		this.lastVehicle = null;
-	}
+    public PlayerEvents() {
+        this.minecraft = Minecraft.getInstance();
+        this.lastVehicle = null;
+    }
 
-	@SubscribeEvent
-	public void onPlayerTick(PlayerTickEvent evt) {
-		if (evt.side.equals(Dist.DEDICATED_SERVER)) {
-			return;
-		}
-		
-		if(!evt.player.equals(getPlayer())){
-			return;
-		}
+    @SubscribeEvent
+    public void onPlayerTick(TickEvent.PlayerTickEvent evt) {
+        if (evt.side.equals(Dist.DEDICATED_SERVER)) {
+            return;
+        }
 
-		EntityVehicleBase vehicle = getRidingVehicle();
-		
-		if (vehicle!=null && lastVehicle==null) {
-			if(vehicle.doesEnterThirdPerson()){
-				setThirdPerson(true);
-			}
-		} else if (vehicle==null && lastVehicle!=null) {
-			if(lastVehicle.doesEnterThirdPerson()){
-				setThirdPerson(false);
-			}
-		}
-		lastVehicle = vehicle;
-	}
+        if (!evt.player.equals(getPlayer())) {
+            return;
+        }
 
-	private void setThirdPerson(boolean third) {
-		if (!Config.thirdPersonEnter.get()) {
-			return;
-		}
+        EntityVehicleBase vehicle = getRidingVehicle();
 
-		if (third) {
-			minecraft.gameSettings.thirdPersonView = 1;
-		} else {
-			minecraft.gameSettings.thirdPersonView = 0;
-		}
+        if (vehicle != null && lastVehicle == null) {
+            if (vehicle.doesEnterThirdPerson()) {
+                setThirdPerson(true);
+            }
+        } else if (vehicle == null && lastVehicle != null) {
+            if (lastVehicle.doesEnterThirdPerson()) {
+                setThirdPerson(false);
+            }
+        }
+        lastVehicle = vehicle;
+    }
 
-	}
+    private void setThirdPerson(boolean third) {
+        if (!Config.thirdPersonEnter.get()) {
+            return;
+        }
 
-	private ClientPlayerEntity getPlayer() {
-		return minecraft.player;
-	}
+        if (third) {
+            minecraft.gameSettings.thirdPersonView = 1;
+        } else {
+            minecraft.gameSettings.thirdPersonView = 0;
+        }
 
-	private Entity getRidingEntity() {
-		return getPlayer().getRidingEntity();
-	}
+    }
 
-	private EntityVehicleBase getRidingVehicle() {
-		Entity e=getRidingEntity();
-		if(e instanceof EntityVehicleBase){
-			return (EntityVehicleBase) e;
-		}
-		
-		return null;
-	}
+    private ClientPlayerEntity getPlayer() {
+        return minecraft.player;
+    }
+
+    private Entity getRidingEntity() {
+        return getPlayer().getRidingEntity();
+    }
+
+    private EntityVehicleBase getRidingVehicle() {
+        Entity e = getRidingEntity();
+        if (e instanceof EntityVehicleBase) {
+            return (EntityVehicleBase) e;
+        }
+
+        return null;
+    }
 }
