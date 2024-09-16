2a78c703fe25dc519da8c4aace55cfc3a9dd9f73
Max Henkel <max@maxhenkel.de>
2024-09-16 12:56:11 +0200

    Fix blocks not rotating when placed with structure blocks
    (cherry picked from commit 33fba9cefb1144544fc1935d4344175265027731)

--- src/main/java/de/maxhenkel/car/blocks/BlockGui.java
+++ src/main/java/de/maxhenkel/car/blocks/BlockGui.java
@@ -13,10 +13,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.EntityBlock;
-import net.minecraft.world.level.block.RenderShape;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -92,6 +89,11 @@ public abstract class BlockGui<T extends BlockEntity> extends BlockBase implemen
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(FACING, POWERED);
+    }
+
+    @Override
+    protected BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
     }
 
     @Override
